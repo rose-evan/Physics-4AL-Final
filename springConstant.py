90b2c52cbd365060ae31cdf52f1b5a6c56813ee1
Evan Rose <139176881+rose-evan@users.noreply.github.com>
2025-05-22 17:36:58 -0700

    spring constants

---
+++ springConstant.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+
+
+# Constants
+
+# Spring constant values
+
+tennisBall_mass = 0.057 # kg
+tennisBall_massError = 0.001 # kg
+
+SpringConstant_noMass = [3, 0.1]
+SpringConstant_Mass = [4.7, 0.1] # mass 400g
+SpringConstant_Mass_2 = [5.8, 0.1] # mass 600g
+SpringConstant_Mass_3 = [7.1, 0.1] # mass 800g
+SpringConstant_Mass_4 = [8.4, 0.1] # mass 1000g
+
+massOfSystem = [1.490, 0.001] #in kg 
+
+print("Hello")
